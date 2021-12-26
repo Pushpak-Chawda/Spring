806379becf929e837966d45371ae7911ea175328
Pushpak-Chawda <pushpaktcas@gmail.com>
2021-12-26 02:22:26 -0500

    Additional commit

--- SpringIOC/src/main/java/com/pc/spring/annotation/initial/MyApp.java
+++ SpringIOC/src/main/java/com/pc/spring/annotation/initial/MyApp.java
@@ -1,15 +1,18 @@
-package com.pc.spring.annotation;
+package com.pc.spring.annotation.initial;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyApp {
 	public static void main(String[] args) {
 
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				"applicationContext_annotation.xml");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext_annotation.xml");
 
-		BaseBallCoach theBBC = context.getBean("baseBallCoach", BaseBallCoach.class);
-		theBBC.m1();
+		
+		 BaseBallCoach theBBC = context.getBean("testBaseBallCoach",
+		 BaseBallCoach.class); 
+		 
+		 System.out.println(theBBC.getName());
+		
 
 		context.close();
 
